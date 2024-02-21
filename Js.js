2daf76f144484c1a41e7df3875a07a694cde2d5e
Khalid <taytaykhalid2001@gmail.com>
2024-02-21 22:44:19 +0100

    project created

---
+++ Js.js
@@ -0,0 +1,33 @@
+let sr = ScrollReveal({
+    distance: '80px',
+    duration: 2000,
+    reset: true
+});
+sr.reveal('h1',{origin:'top'});
+sr.reveal('h2',{origin:'top'});
+sr.reveal('button', { origin: 'right' });
+sr.reveal('.icons', { origin: 'left' });
+sr.reveal('.About-me', { origin: 'left' });
+sr.reveal('.About-me-img', { origin: 'right' });
+sr.reveal('.About-me-img', { origin: 'right' });
+
+//skills
+sr.reveal('.My-skills', { origin: 'right' });
+sr.reveal('#skill1', { origin: 'right',});
+sr.reveal('#skill2', { origin: 'left' });
+sr.reveal('#skill3', { origin: 'right' });
+sr.reveal('#skill4', { origin: 'left' });
+sr.reveal('#skill5', { origin: 'right' });
+sr.reveal('#skill6', { origin: 'left' });
+
+//Projects
+
+sr.reveal('#img1',);
+sr.reveal('#img2', { delay:100 });
+sr.reveal('#img3', { delay:200 });
+sr.reveal('#img4', { delay:300 });
+sr.reveal('#img5', { delay:400 });
+
+sr.reveal('.text',{origin:'left'});
+
+
